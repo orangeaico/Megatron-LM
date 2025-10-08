d794c7f0c0e7298c96a8e3d03ea11a587554fe64
Himanshu Agarwal <himanshu@livup.ai>
2025-10-08 15:49:11 +0530

    Updated vast script to remove logs

--- setup_vast_container.sh
+++ setup_vast_container.sh
@@ -20,8 +20,6 @@ mkdir -p /workspace/data/
 echo "Megatron-LM setup complete without data setup!"
 
 if [ "$SETUP_DATA" -eq 1 ]; then
-echo "Setting up rclone and copying data and models from gdrive.."
-
 echo "Installing rclone.."
 apt-get update -y
 apt-get install -y rclone
@@ -36,7 +34,7 @@ type = drive
 scope = drive
 client_id = 983615320622-9vfjc78upb9igrcf54i6dvb4cvecfpm3.apps.googleusercontent.com
 client_secret = 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
-token = {}
+token = {"access_token":"ya29.a0AQQ_BDQc3eRiMLbMhOhLx20QyfOwSeY1iBlCTY2sG7ox-7Drn3F_H_JGBOqw7__z_5KebyxKo6KHb1k16J4KfWNXZmFiwGmyzTtUYb-OT61lmeF6lG2mmx_o9wbEDf0jBDO7NjeLW2-WrvIQsZOD06nCz6v5ez4SCoq0chJcZTpNSrV0AGLq0KHc94uF4OCEOJARSQoaCgYKAQMSARcSFQHGX2MilmEFQecTGlDBnGElN5lOCA0206","token_type":"Bearer","refresh_token":"1//0gt3EUPKSqbWrCgYIARAAGBASNwF-L9Irwi-Axig2zM4UoNRQ5ZprohdBfFO8R3EIK6Nq-trkZxvCMFHiFLR7Od4BuqzzwFsiDG4","expiry":"2025-10-08T16:40:04.545814+05:30","expires_in":3599}
 EOF
 
 chmod 600 "$CONFIG_PATH"
@@ -48,8 +46,6 @@ rclone about gdrive: -vv
 
 echo "🎉 Google Drive remote [gdrive] is ready!"
 
-echo "Copying Files from gdrive to /workspace/data/"
-
 cd /workspace/data/
 
 echo "Copying data to /workspace/data/"
@@ -60,7 +56,6 @@ rclone copy -P gdrive:"megatron_dir/mega-models/Qwen3-Coder-30B-A3B-Instruct-tor
 
 cd /workspace/Megatron-LM/
 echo "Data copying complete!"
-echo "Changed working directory to /workspace/Megatron-LM/"
 fi
 
 echo "All Done!"
